3155b74f8111a5262fa6f6aa45bbf275a9be7dba
0Armaan025 <armaan33000@gmail.com>
2023-06-24 21:02:56 +0530

    changed build.gradle and pubpsec

--- lib/features/onboarding/screens/onboarding_screen.dart
+++ lib/features/onboarding/screens/onboarding_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:skillconnect/common/utils.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -11,6 +11,32 @@ class OnBoardingScreen extends StatefulWidget {
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size(double.infinity, kToolbarHeight),
+        child: makeAppBar(context),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: size.height * 0.6,
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/connection.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skillconnect/features/onboarding/screens/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,12 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text("SkillConnect!"),
-          //connecting my phone to check if this works!!!
-        ),
-      ),
+      home: const OnBoardingScreen(),
     );
   }
 }
