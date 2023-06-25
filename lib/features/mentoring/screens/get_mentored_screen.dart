41d068c901f474d20f7576702adeebd0efd64776
0Armaan025 <armaan33000@gmail.com>
2023-06-25 08:51:22 +0530

    commiting

---
+++ lib/features/mentoring/screens/get_mentored_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../../common/constants.dart';
+import '../../../common/utils.dart';
+
+class GetMentoredScreen extends StatefulWidget {
+  const GetMentoredScreen({super.key});
+
+  @override
+  State<GetMentoredScreen> createState() => GetMentoredScreenState();
+}
+
+class GetMentoredScreenState extends State<GetMentoredScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      drawer: buildDrawer(),
+      appBar: PreferredSize(
+        preferredSize: const Size(double.infinity, kToolbarHeight),
+        child: makeAppBar(context),
+      ),
+    );
+  }
+}
